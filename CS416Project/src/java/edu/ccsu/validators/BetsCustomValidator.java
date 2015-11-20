7af07501e81463c19585b14f132d63a65e2edafd
KKonopka <kkonopka@my.ccsu.edu>
2015-11-20 03:00:51 -0500

    dfgdsf

---
+++ CS416Project/src/java/edu/ccsu/validators/BetsCustomValidator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ccsu.validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author cw1491
+ */
+@FacesValidator(value="betsCustomValidator")
+public class BetsCustomValidator implements Validator{
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String bet = (String)value;
+        double Dbet = Double.parseDouble(bet);
+        HtmlInputText htmlInputText = (HtmlInputText)component;
+        if (Dbet<0){
+            FacesMessage facesMessage =  new FacesMessage("You can't bet negative money!");
+            throw new ValidatorException(facesMessage);
+        }
+        if (bet.matches("0-9")){
+            FacesMessage facesMessage =  new FacesMessage("Bet must be a numeric value!");
+            throw new ValidatorException(facesMessage);
+     
+    }
+}
+}
